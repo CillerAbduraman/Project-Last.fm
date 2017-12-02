4cd0e67091842660b6e4eb6963ee6ab592d5cb12
CillerAbduraman <32862283+CillerAbduraman@users.noreply.github.com>
2017-12-02 12:14:22 +0200

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -35,10 +35,10 @@ CREATE TABLE IF NOT EXISTS `artists` (
 
 CREATE TABLE IF NOT EXISTS `songs` (
   `id` smallint(5) NOT NULL AUTO_INCREMENT,
-  `genreId` smallint(5) NOT NULL,
-  `artistId` smallint(5) NOT NULL,
+  `genreId` smallint(5) DEFAULT NULL,
+  `artistId` smallint(5) DEFAULT NULL,
   `name` varchar(30) DEFAULT NULL,
-  `fileName` text NOT NULL,
+  `fileName` varchar(100) DEFAULT NULL,
   `playtime` smallint(5) DEFAULT NULL,
   `createdAt` timestamp,
   `updatedAt` timestamp,
@@ -46,14 +46,14 @@ CREATE TABLE IF NOT EXISTS `songs` (
   KEY `id` (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- Table structure for table `users`
+-- Table structure for table `users` - I don't know if I'm gonna use it, because until now I have just one user
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` smallint(5) NOT NULL AUTO_INCREMENT,
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   `email` varchar(30) DEFAULT NULL,
-  `image` text NOT NULL NULL,
+  `image` varchar(100) DEFAULT NULL,
   `createdAt` timestamp,
   `updatedAt` timestamp,
   PRIMARY KEY (`id`),
