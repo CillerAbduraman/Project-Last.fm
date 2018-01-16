b3ac265af4bd761eb5023fa5e2900b3e0460701f
CillerAbduraman <32862283+CillerAbduraman@users.noreply.github.com>
2018-01-16 07:31:28 +0200

    Create topArtists.jsx

---
+++ views/topArtists.jsx
@@ -0,0 +1,80 @@
+var React = require('react');
+var createReactClass = require('create-react-class');
+var TopTemplate = require('./layout/topTemplate');
+var hidden = {
+    display:'none'
+};
+
+var container = {
+  display: 'inline-block',
+  width : '100%',
+  height: '174px'
+  
+}
+
+var headline = {
+  padding: '0px 30px',
+  display: 'inline-block',
+  fontSize : '24px',
+  color : '#ffffff'
+}
+
+var subheadline = {
+  fontSize : '18px',
+  color : '#aaaaaa'
+}
+
+var photoBox = {
+  verticalAlign: 'middle',
+  display: 'inline-block',
+  height: '174px',
+  width: '174px'
+};
+
+export default class topArtists extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  
+
+  render() {
+      var listItems = this.props.data.map(function(item){
+        return (
+                <tr>
+                    <form action="/addArtist" method="post">
+                    <td>
+                        <div style={container}>
+                            <div style={photoBox}>
+                              <a href={item.url}> <img src={item.image[2]['#text']}/></a>
+                            </div>
+                            
+                            <div style={headline}>
+                              {item.name}
+                            </div>
+                            
+                            <input type="text" name="url" value={item.url} style={hidden} />
+                            <input type="text" name="poza" value={item.image[2]['#text']} style={hidden} />
+                            <input type="text" name="ascultatori" value={item.listeners} style={hidden} />
+                            <input type="text" name="nume" value={item.name} style={hidden} />
+                            <input type="submit" value="Add to favorite artists" />
+                        </div>
+                      </td>
+                    </form>
+                </tr>
+            ); 
+        });
+        
+    return (
+      <TopTemplate title='Top Artists'>
+        <table>
+                {listItems}
+        </table>
+      </TopTemplate>
+    );
+  }
+}
